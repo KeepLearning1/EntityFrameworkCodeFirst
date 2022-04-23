7e685e5ad5863039b203c84a69002c37262190b5
venkatesh k <venkateshkandregula891@outlook.com>
2022-04-23 15:07:05 +0530

    added comments about migrations

--- EFCF1/DataContext.cs
+++ EFCF1/DataContext.cs
@@ -19,3 +19,12 @@ namespace EFCF1
         public DbSet<Company> Company { get; set; }
     }
 }
+
+///Summary///
+///enable-migrations///-to Enables the migration for the project by creating a Configuration class.
+///add-migration///-Creates a new migration class as per specified name with the Up() and Down() methods
+///update-database///-Executes the last migration file created by the Add-Migration command and applies changes to the database schema.
+///update-database -Verbose///Use the –verbose option to view the SQL statements being applied to the target database.
+///update-database -TargetMigration:SchoolDB-v1//to roll back the database schema to any of the previous states, 
+///        then you can execute the update-database command with the –TargetMigration parameter to the point which you want to roll back to
+///        ex: update-database -TargetMigration:202204230757439_data1
