28f3f127cee868f22ab39cc9e9680d5ea9acd01d
venkatesh k <venkateshkandregula891@outlook.com>
2022-04-24 14:22:36 +0530

    Table per Hierachy EFCF

---
+++ vfweeuonwiojiwrovijvwr/Migrations/202204240828589_add1.cs
@@ -0,0 +1,38 @@
+﻿namespace vfweeuonwiojiwrovijvwr.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class add1 : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.PersonalDetails",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        Age = c.Int(nullable: false),
+                        StreetName = c.String(),
+                        City = c.String(),
+                        State = c.String(),
+                        ZipCode = c.String(),
+                        StaffID = c.Int(),
+                        StaffName = c.String(),
+                        StudentID = c.Int(),
+                        StudentName = c.String(),                     
+                    })
+                .PrimaryKey(t => t.ID)
+                .Index(t => t.StaffID, unique: true, name: "INDEX_STAFFID")
+                .Index(t => t.StudentID, unique: true, name: "INDEX_STUDENTID");
+            
+        }
+        
+        public override void Down()
+        {
+            DropIndex("dbo.PersonalDetails", "INDEX_STUDENTID");
+            DropIndex("dbo.PersonalDetails", "INDEX_STAFFID");
+            DropTable("dbo.PersonalDetails");
+        }
+    }
+}
