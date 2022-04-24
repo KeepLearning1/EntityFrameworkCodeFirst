daa7c50a38a5ce60ffc0b34f490444f6cfcb6fd3
venkatesh k <venkateshkandregula891@outlook.com>
2022-04-24 14:27:13 +0530

    changed file name

---
+++ EFCFTPH/Staff.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCFTPH
+{
+    class Staff : PersonalDetail
+    {
+        [Index("INDEX_STAFFID",  IsUnique = true)]
+        public int StaffID { get; set; }
+
+        public string StaffName { get; set; }
+
+    }
+}
---
+++ EFCFTPH/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCFTPH
+{
+    class Student : PersonalDetail
+    {
+        [Index("INDEX_STUDENTID", IsUnique = true)]
+        public int StudentID { get; set; }
+
+        public string StudentName { get; set; }
+
+    }
+}
